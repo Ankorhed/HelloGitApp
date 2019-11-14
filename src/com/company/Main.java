e212ed234ce1ac0dfdc792825cbaa98f0fa07885
Markus Nussbaumer <markus.nussbaumer.mn@gmail.com>
2019-11-14 21:12:51 +0100

    3nd Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello GIT!");
         System.out.println("Zweite Ausgabezeile!");
-        // test
+        // test2
     }
 }
 
